540b1db8fdaf0b3e8a098fbec5d4d929855a9737
quocduy2003 <duy.tq22942@sinhvien.hoasen.edu.vn>
2024-10-23 07:25:04 +0700

    create home page

--- frontend_MyCV/index.js
+++ frontend_MyCV/index.js
@@ -2,10 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 import App from './App';
 import CreateEmployer from './src/screen/Employer/CreateEmployer';
 import JobPost from './src/screen/Employer/JobPost';
+import Navbar from './src/components/Navbar';
+import Home from './src/screen/Home/Home';
 
-AppRegistry.registerComponent(appName, () => JobPost);
+// Register the app with TypeScript support
+AppRegistry.registerComponent(appName, () => Home);
