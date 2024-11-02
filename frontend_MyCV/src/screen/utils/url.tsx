28db9b28936768eda8c71e19ff2267dfe02ed23b
Gr1USGD <uyen.mt40156@sinhvien.hoasen.edu.vn>
2024-11-02 09:09:37 +0700

    'uyen'

--- frontend_MyCV/src/screen/utils/url.tsx
+++ frontend_MyCV/src/screen/utils/url.tsx
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const BASE_URL ='http://192.168.58.88:3000'
-=======
-export const BASE_URL ='http://192.168.0.115:3000'
->>>>>>> b2c5f0359f498ea3f1e20e2224daa20db0bde9bb
+export const BASE_URL = 'http://10.106.21.199:3000'
