815da850c2bcad322c8b68eaf7d279af79a90417
Gr1USGD <uyen.mt40156@sinhvien.hoasen.edu.vn>
2024-12-04 11:52:17 +0700

    'uyen-update'

--- backend_MyCV/src/cv/cv.module.ts
+++ backend_MyCV/src/cv/cv.module.ts
@@ -3,10 +3,12 @@ import { CvService } from './cv.service';
 import { CvController } from './cv.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Cv, CvSchema } from './entities/cv.entity';
+import { ApplicationsModule } from 'src/applications/applications.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Cv.name, schema: CvSchema }]),
+    ApplicationsModule
   ],
   controllers: [CvController],
   providers: [CvService],
